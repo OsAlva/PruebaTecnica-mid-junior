a1af51237de3e30d6488c248da1ebd72f4600d86
Oscar Alva <oscaraldrin23@gmail.com>
2023-06-01 16:30:37 +0200

    desarrollando el styled components header con vanilla-extract

---
+++ src/components/Header.css.ts
@@ -0,0 +1,41 @@
+import {style} from '@vanilla-extract/css';
+
+export const headerContainer = style({
+    backgroundColor: '#ff6600',
+
+})
+
+export const header = style({
+    alignItems: 'center',
+    borderBottom: '1px solid #ccc',
+    display: 'flex',
+    gap: '20px',
+    padding: '0 32px'
+});
+
+export const logo = style({
+    maxWidth: '30px',
+})
+
+export const link = style({
+    textDecoration: 'none',
+    color: 'inherit',
+    fontSize: '1.2rem',
+    margin: '0',
+
+})
+
+
+export const listUl = style({
+    textDecoration: 'none',
+    display: 'flex',
+    gap: '20px',
+    justifyContent: 'space-between',
+    listStyle: 'none',
+})
+
+
+export const headerOption = style({
+    textDecoration: 'none',
+    color: 'inherit',
+})
---
+++ src/components/Header.tsx
@@ -0,0 +1,23 @@
+import {header, link, listUl, headerOption, headerContainer, logo} from './Header.css';
+
+export const Header = () => {
+
+    return (
+        <div className={headerContainer}>
+            <nav className={header}>
+                <img className={logo} src="../public/y.svg" alt="logo"/>
+                <a className={link} href="/"> Hacker News</a>
+                <ul className={listUl}>
+                    <li><a className={headerOption} href="#">new</a></li>
+                    <li><a className={headerOption} href="#">past</a></li>
+                    <li><a className={headerOption} href="#">comments</a></li>
+                    <li><a className={headerOption} href="#">ask</a></li>
+                    <li><a className={headerOption} href="#">show</a></li>
+                    <li><a className={headerOption} href="#">jobs</a></li>
+                    <li><a className={headerOption} href="#">submit</a></li>
+                </ul>
+            </nav>
+        </div>
+    )
+
+}
