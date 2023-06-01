66fcacfcaf219a0cb698b945c3acbaa62d60ce77
Oscar Alva <oscaraldrin23@gmail.com>
2023-06-01 16:59:43 +0200

    hacemos lazy load para que solo cargue lo que necesitamos nuestra aplicacion

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,9 @@
-import { lazy } from "react";
+import { Suspense, lazy } from "react";
 import { Route } from "wouter";
 import './App.css'
 import { Header } from './components/Header'
 // import TopStories from "./pages/TopStories";
 // import Detail from "./pages/Detail";
-
-
 
 //para que no carguen todas las paginas creamos un Lazy load que solo cargue la pagina que necesitamos
 const TopStories = lazy(() => import('./pages/TopStories'))
@@ -17,8 +15,11 @@ function App() {
   return (
     <>
      <Header />
-     <Route path="/" component={TopStories} />
-     <Route path="/article/:id" component={Detail}/>
+
+    <Suspense fallback={null}> {/* envolvemos en un suspense los componentes para que no de error*/}
+      <Route path="/" component={TopStories} />
+      <Route path="/article/:id" component={Detail}/>
+     </Suspense>
     </>
     
   )
